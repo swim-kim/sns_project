ed4627c7e47bf951f63b2a138cb517a6ed1e9d03
swimkim-01 <ksoo5386@naver.com>
2021-05-31 23:45:36 +0900

    first commit

---
+++ main/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from .views import *
+
+app_name = "main"
+urlpatterns = [
+    path('', showmain, name="showmain"),
+    path('first/',first, name="first"),
+    path('second/',second, name="second"),
+    path('<str:id>',detail, name="detail"),
+    path('new/', new, name="new"),
+    path('create/',create, name="create"),
+    path('edit/<str:id>',edit, name="edit"),
+    path('update/<str:id>',update, name="update"),
+    path('delete/<str:id>', delete, name="delete"),
+]
--- sns_project/settings.py
+++ sns_project/settings.py
@@ -128,5 +128,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'sns_project', 'static')
 ]
-
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
